58591dd47478acff96b33757d44e1454495a1f07
archersh <archersh@hotmail.com>
2019-04-10 09:49:58 +0800

    config

---
+++ compassrest/src/main/java/cn/com/sailin/compassrest/config/ScheduleConfig.java
@@ -0,0 +1,17 @@
+package cn.com.sailin.compassrest.config;
+
+import java.util.concurrent.Executors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer{
+	
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+		scheduledTaskRegistrar.setScheduler(Executors.newScheduledThreadPool(10));
+	}
+
+}
